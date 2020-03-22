a6fd0fca32c3fb667a580cb6deb642ac95d2f0ba
xavierhazzardadmin <xavier@hazzard.org.uk>
2020-03-22 19:10:36 +0000

    Finished Change Styling lectures

---
+++ app.js
@@ -0,0 +1,15 @@
+var isFirstVisit;
+
+const testReturn = cond => {
+  var welcomeMsg = "Welcome to node!";
+
+  isFirstVisit = cond;
+
+  if (isFirstVisit == true) {
+    return welcomeMsg;
+  } else {
+    return "Welcome back!";
+  }
+};
+
+console.log(testReturn(true));
