ddf498dad9cda2fed58ad9cd4140568c1feece01
xavierhazzardadmin <xavier@hazzard.org.uk>
2020-04-13 13:29:26 +0100

    Changed titles to HTML files and built countdown

--- app.js
+++ app.js
@@ -14,17 +14,35 @@
 
 // console.log(testReturn(true));
 
-var messages = [
-  "",
-  "Hi there!",
-  "Welcome",
-  "Hello there",
-  "Whats up",
-  "Heyyyyyyyyyyyy",
-  "Sup",
-  "Welcome",
-];
+// var messages = [
+//   "",
+//   "Hi there!",
+//   "Welcome",
+//   "Hello there",
+//   "Whats up",
+//   "Heyyyyyyyyyyyy",
+//   "Sup",
+//   "Welcome",
+// ];
 
-var index = Math.floor(7 * Math.random());
+// var index = Math.floor(7 * Math.random());
 
-console.log(messages[index]);
+// console.log(messages[index]);
+
+function countBack() {
+  var msg = "Jehovah's people will be back together in: ";
+  var i = 100;
+
+  var count = setInterval(() => {
+    console.log(msg + i + " days");
+    i--;
+
+    if (i < 1) {
+      clearInterval(count);
+    }
+  }, 500);
+}
+
+console.log("hi");
+
+countBack();
